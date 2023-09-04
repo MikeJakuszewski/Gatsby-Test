85c3cd15efa7760c98a5d361f0f2ab2272c6c87e
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-09-04 13:16:32 -0400

    added plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import { Link, type HeadFC, type PageProps } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
         <h4>Welcome to my Gatsby site!</h4>
-
         <p>I'm learning Gatsby</p>
+        <StaticImage
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+          alt="Clifford"
+        />
+
+        <Link to="https://www.google.com">
+          <StaticImage src="../assets/Bills.png" alt="Bills" />
+        </Link>
       </Layout>
     </main>
   );
