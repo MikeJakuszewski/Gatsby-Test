2127c6b6f8a6fbdb6abab1ac1a7993ebb04b081c
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-09-04 13:29:45 -0400

    fixed file name bug preventing site deployment

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,12 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify",
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link, type HeadFC, type PageProps } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage: React.FC<PageProps> = () => {
